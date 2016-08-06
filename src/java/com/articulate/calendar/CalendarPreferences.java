17004a5bb81e8c768cadbe93b582925c42d3664d
Jeff Thompson <jeff@thefirst.org>
2016-08-06 16:45:50 +0200

    Added CalendarPrefereince.

---
+++ src/java/com/articulate/calendar/CalendarPreferences.java
@@ -0,0 +1,31 @@
+/* This code is copyright Articulate Software (c) 2016.  Some
+portions copyright Teknowledge (c) 2003 and reused under the terms of
+the GNU license.  This software is released under the GNU Public
+License <http://www.gnu.org/copyleft/gpl.html>.  Users of this code
+also consent, by use of this code, to credit Articulate Software and
+Teknowledge in any writings, briefings, publications, presentations,
+or other representations of any software which incorporates, builds
+on, or uses this code.
+*/
+
+package com.articulate.calendar;
+
+import java.time.DayOfWeek;
+import java.util.TimeZone;
+
+/**
+ * A CalendarPreferences holds user preferences for the Calendar application.
+ * @author Jeff Thompson, jeff@thefirst.org
+ */
+public class CalendarPreferences {
+  public TimeZone getTimeZone() { return timeZone_; }
+
+  /**
+   * Get the start day of the week for displaying a week.
+   * @return The start day as a DayOfWeek.
+   */
+  public DayOfWeek getStartOfWeek() { return startOfWeek_; }
+  
+  private TimeZone timeZone_ = TimeZone.getDefault();
+  private DayOfWeek startOfWeek_ = DayOfWeek.MONDAY;
+}
