24cff336f87b3e4d49e8960b64df21c82b0486e9
jefft0 <jefft0@sample.com>
2017-02-19 23:08:14 +0100

    CalendarApp: Temporary code to process Wikidata and write locationIanaTimeZone.kif, etc.

--- src/java/com/articulate/calendar/CalendarApp.java
+++ src/java/com/articulate/calendar/CalendarApp.java
@@ -10,14 +10,25 @@ on, or uses this code.
 
 package com.articulate.calendar;
 
+import com.articulate.calendar.WikidataJava.Item;
 import com.articulate.calendar.argue.Argument;
 import com.articulate.calendar.argue.ArgumentSet;
 import com.articulate.calendar.gui.CalendarFrame;
 import com.articulate.sigma.Formula;
 import com.articulate.sigma.KBmanager;
 import com.articulate.sigma.WordNet;
+import com.google.gson.Gson;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * CalendarApp has the main method which creates the main window and starts the
@@ -29,7 +40,60 @@ public class CalendarApp {
    * This the main entry for the application.
    * @param args The command line arguments.
    */
-  public static void main (String args[]) {
+  public static void main (String args[]) 
+      throws FileNotFoundException, IOException 
+  {
+/*
+    String dumpDir = "/home/jeff/temp/";
+    ArrayList<String> messages = new ArrayList<>();
+    boolean doDump = false;
+    if (doDump) {
+      WikidataJava.dumpFromJson("/home/jeff/temp/wikidata-20170213-all.json.gz", dumpDir, messages);
+      for (String message : messages)
+        System.out.println(message);
+      if (true) return;
+    }
+
+    WikidataJava wikidata = new WikidataJava(dumpDir);
+    WikidataJava.getStatistics(wikidata.items_, messages);
+
+    try (FileWriter file = new FileWriter(new File(dumpDir, "locationIanaTimeZone.kif").getAbsolutePath());
+         BufferedWriter writer = new BufferedWriter(file)) {
+      Set<String> timeZoneMessages = new HashSet<>();
+      Map<Integer, Integer> map = WikidataJava.getLocationIanaTimeZones
+        (wikidata.items_, timeZoneMessages);
+      System.out.println("Debug locationIanaTimeZone size " + map.size());
+      for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        writer.write
+          ("(locationIanaTimeZone Q" + entry.getKey() + " Q" + entry.getValue() +
+           ")");
+        writer.newLine();
+      }
+
+      for (String message : timeZoneMessages)
+        System.out.println(message);
+    }
+
+    for (String message : messages)
+      System.out.println(message);
+
+    Gson gson = new Gson();
+    try (FileWriter file = new FileWriter(new File(dumpDir, "iataAbbreviation.kif").getAbsolutePath());
+         BufferedWriter writer = new BufferedWriter(file)) {
+      for (Map.Entry<Integer, Item> entry : wikidata.items_.entrySet()) {
+        Item item = entry.getValue();
+        if (item.iataAirportCode_ != null && item.iataAirportCode_.length == 1) {
+          writer.write
+            ("(abbreviation " + gson.toJson(item.iataAirportCode_[0]) +
+             " Q" + entry.getKey() + ")");
+          writer.newLine();
+        }
+      }
+    }
+
+    if (true) return;
+*/
+    
     KBmanager.getMgr().initializeOnce();
     WordNet.initOnce();
     CalendarPreferences preferences = new CalendarPreferences();
@@ -37,6 +101,34 @@ public class CalendarApp {
     CalendarKB calendarKB = new CalendarKB(KBmanager.getMgr().getKB("SUMO"));
     // Pre-cache overlapsDate results now.
     calendarKB.overlapsDate(LocalDate.now(), preferences.getTimeZone());
+
+    try (FileWriter file = new FileWriter("/home/jeff/temp/debugAllLocations.txt");
+         BufferedWriter writer = new BufferedWriter(file)) {
+      for (Map.Entry<String, String> entry : calendarKB.locationIanaTimeZone_.entrySet()) {
+        String locationLabel = calendarKB.itemTermFormatEnglishLanguage_.get(entry.getKey());
+        if (locationLabel == null || locationLabel.isEmpty())
+          // No label for the location.
+          continue;
+        String timeZoneLabel = calendarKB.itemTermFormatEnglishLanguage_.get(entry.getValue());
+        if (timeZoneLabel == null || timeZoneLabel.isEmpty())
+          // No label for the time zone.
+          continue;
+
+        writer.write(timeZoneLabel + " " + locationLabel);
+        writer.newLine();
+      }
+      for (Map.Entry<String, String> entry : calendarKB.iataAbbreviation_.entrySet()) {
+        if (!calendarKB.locationIanaTimeZone_.containsKey(entry.getKey()))
+          // No location for the airport.
+          continue;
+        String timeZoneLabel = calendarKB.itemTermFormatEnglishLanguage_.get(calendarKB.locationIanaTimeZone_.get(entry.getKey()));
+        if (timeZoneLabel == null || timeZoneLabel.isEmpty())
+          // No label for the time zone.
+          continue;
+        writer.write(timeZoneLabel + " " + entry.getValue());
+        writer.newLine();
+      }
+    }
 
     ArgumentSet argumentSet = makeArgumentSet(calendarKB);
 
